80d2939ef8bb5689bc0ad92bd64cccb09ccdf8b7
Stuart Read <stuart.read@lightsource.ca>
2018-07-26 10:13:55 -0600

    test_tile_reader: add PreprocessorList test

--- orangecontrib/protospec/tests/test_tile_reader.py
+++ orangecontrib/protospec/tests/test_tile_reader.py
@@ -3,6 +3,7 @@ import unittest
 
 import Orange
 from Orange.data.io import FileFormat
+from Orange.preprocess.preprocess import PreprocessorList
 
 from orangecontrib.spectroscopy import get_sample_datasets_dir
 from orangecontrib.spectroscopy.tests.test_preprocess import PREPROCESSORS_INDEPENDENT_SAMPLES
@@ -30,3 +31,12 @@ class TestTilePreprocessors(unittest.TestCase):
         for p in PREPROCESSORS_INDEPENDENT_SAMPLES:
             reader.set_preprocessor(p)
             reader.read()
+
+    def test_preprocessor_list(self):
+        # TODO problematic interface design: should be able to use Orange.data.Table directly
+        path = os.path.join(get_sample_datasets_dir(), AGILENT_TILE)
+        reader = FileFormat.get_reader(path)
+        pp = PreprocessorList(PREPROCESSORS_INDEPENDENT_SAMPLES[0:7])
+        reader.set_preprocessor(pp)
+        t = reader.read()
+        assert len(t.domain.attributes) == 3
