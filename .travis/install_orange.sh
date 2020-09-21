1773da2044567f8c3cfb9675d0dd3aa3f01f1821
Stuart Read <stuart.read@lightsource.ca>
2020-09-21 15:41:54 -0600

    tests: update to match orange-spec

--- .travis/install_orange.sh
+++ .travis/install_orange.sh
@@ -1,9 +1,3 @@
-# CommonMark changed their interface in 0.8.0. Because older
-# Orange is imcompatible with newer CommonMark,
-# install the module manually so that it does not get installed.
-# Remove this when the minimum supported Orange is 3.16.
-pip install CommonMark==0.7.5
-
 if [ $ORANGE == "release" ]; then
     echo "Orange: Skipping separate Orange install"
     return 0
@@ -11,7 +5,7 @@ fi
 
 if [ $ORANGE == "master" ]; then
     echo "Orange: from git master"
-    pip install https://github.com/biolab/orange3/archive/master.zip
+    pip install https://github.com/biolab/orange-canvas-core/archive/master.zip https://github.com/biolab/orange-widget-base/archive/master.zip https://github.com/biolab/orange3/archive/master.zip
     return $?;
 fi
 
--- .travis/stage_script.sh
+++ .travis/stage_script.sh
@@ -9,5 +9,11 @@ if [ "$RUN_PYLINT" ]; then
 else
     # Screen must be 24bpp lest pyqt5 crashes, see pytest-dev/pytest-qt/35
     export XVFBARGS="-screen 0 1280x1024x24"
-    catchsegv xvfb-run -a -s "$XVFBARGS" python setup.py test
+    catchsegv xvfb-run -a -s "$XVFBARGS" coverage run -m unittest
+    (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
+fi
+
+if [ "$UPLOAD_COVERAGE" ]; then
+    cp $TRAVIS_BUILD_DIR/codecov.yml .
+    codecov
 fi
