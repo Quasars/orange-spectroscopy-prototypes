25491c357b9ed72d8d82ec86154503e06ecb34a1
Stuart Read <stuart.read@lightsource.ca>
2024-11-19 10:31:49 -0600

    [TEST] Don't import tests from another package

--- orangecontrib/protospec/tests/test_io.py
+++ orangecontrib/protospec/tests/test_io.py
@@ -1,12 +1,37 @@
-import numpy as np
+import unittest
 
-from Orange.data.tests.test_io import TestWriters
+import numpy as np
+from Orange.data import (
+    Domain,
+    DiscreteVariable,
+    ContinuousVariable,
+    StringVariable,
+    Table,
+)
+
 from Orange.tests import named_file
 
 from orangecontrib.protospec.data import HDF5Reader
 
 
-class TestHDF5(TestWriters):
+class TestHDF5(unittest.TestCase):
+    # From Orange.data.test.test_io.TestWriters
+    def setUp(self):
+        self.domain = Domain(
+            [
+                DiscreteVariable("a", values=tuple("xyz")),
+                ContinuousVariable("b", number_of_decimals=3),
+            ],
+            ContinuousVariable("c", number_of_decimals=0),
+            [StringVariable("d")],
+        )
+        self.data = Table.from_numpy(
+            self.domain,
+            np.array([[1, 0.5], [2, np.nan], [np.nan, 1.0625]]),
+            np.array([3, 1, 7]),
+            np.array([["foo", "bar", np.nan]], dtype=object).T,
+        )
+
     def test_roundtrip_hdf5(self):
         with named_file('', suffix='.hdf5') as fn:
             HDF5Reader.write(fn, self.data)
